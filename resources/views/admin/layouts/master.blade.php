6272068c3cfcbbbd4450c0c2fb8c37a3f96e0188
nayeem <nayeemsarwar1275@gmail.com>
2023-01-22 17:20:23 +0600

    ..

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,50 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- style  --}}
-        @include('admin.layouts.style')
+    <title>@yield('title') - {{ config('app.name', 'Laravel') }}</title>
 
-        {{-- toastr style  --}}
-        <link rel="stylesheet" href="{{asset('massage/toastr/toastr.css')}}">
-        {{-- custom style  --}}
-        @stack('style')
+    {{-- style  --}}
+    @include('admin.layouts.style')
 
-    </head>
-    <body class="hold-transition sidebar-mini">
-        {{-- header area  --}}
-        @include('admin.layouts.header')
-        {{-- sidebar area  --}}
-        @include('admin.layouts.sidebar')
-        {{-- main content  --}}
-        @yield('content')
-        {{-- footer  --}}
-        @include('admin.layouts.footer')
+    {{-- toastr style  --}}
+    <link rel="stylesheet" href="{{ asset('massage/toastr/toastr.css') }}">
+    {{-- custom style  --}}
+    @stack('style')
 
-        {{-- javascript  --}}
-        @include('admin.layouts.script')
+</head>
 
-        {{-- toastr javascript --}}
-        <script src="{{asset('massage/toastr/toastr.js')}}"></script>
-        {!! Toastr::message() !!}
-        <script>
-            @if($errors->any())
-                @foreach($errors->all() as $error)
-                    toastr.error('{{ $error }}','Error',{
-                        closeButton:true,
-                        progressBar:true,
-                    });
-                @endforeach
-            @endif
-        </script>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 
-        {{-- custom js area  --}}
-        @stack('script')
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-dark">
+            @include('admin.layouts.header')
+        </nav>
+        <!-- /.navbar -->
 
-    </body>
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            @include('admin.layouts.sidebar')
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            @include('admin.layouts.footer')
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+    @include('admin.layouts.script')
+
+    {{-- toastr javascript --}}
+    <script src="{{ asset('massage/toastr/toastr.js') }}"></script>
+    {!! Toastr::message() !!}
+    <script>
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.error('{{ $error }}', 'Error', {
+                    closeButton: true,
+                    progressBar: true,
+                });
+            @endforeach
+        @endif
+    </script>
+
+    {{-- custom js area  --}}
+    @stack('script')
+
+</body>
+
 </html>
